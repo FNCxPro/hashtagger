779579a2f93c0883ab962b34f9f3a2e857f67959
Seth Stephens <relative@airmail.cc>
2018-05-07 19:37:15 -0400

    Hashtag parsing works somewhat, but not all of the way.

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const Commando = require('discord.js-commando'),
       Sequelize = require('sequelize'),
       Discord = require('discord.js'),
       config = require('config'),
+      moment = require('moment'),
       log = require('./logger'),
       path = require('path')
 
 const client = new Commando.CommandoClient({
   owner: config.get('bot.owner'),
-  commandPrefix: config.get('bot.prefix')
+  commandPrefix: config.get('bot.prefix'),
+  disableEveryone: true // Don't want people hashtagging and using @everyone :(
 })
 
-const database = require('./models')
+const db = require('./models')
 
 client.on('ready', () => {
   log.info(`${client.user.username} is ready on ${client.guilds.size} servers and ${client.channels.size} channels.`)
@@ -19,13 +21,54 @@ client.on('ready', () => {
 client.on('message', async (message) => {
   if (message.channel.type !== 'dm' && message.guild) {
     let guild = message.guild
-    // Find or create the guild in the database. It won't be used after it's found however.
+    // Find or create the guild in the database. I lied, it will be used to see if the server was setup for a hashtag yet.
     // TODO: see about optimization
-    let dguild = await database.Server.findOrCreate({
+    let _dguild = await db.Server.findOrCreate({
       where: {
         id: guild.id
       }
     })
+    let dguild = _dguild[0]
+    try {
+      if (dguild.setup) {
+        let content = message.cleanContent
+        let matches = content.match(/#([A-Za-z0-9]+)/gi)
+        if (!matches) return
+        let wotags = matches.map((x) => x.substr(1))
+        wotags.forEach(async (ht) => {
+          let quote = new Discord.RichEmbed()
+            .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL) // Set embed author to the author of the quote
+            .setTimestamp(moment(message.createdTimestamp).toISOString()) // Set embed timestamp to when the message was authored
+            .setColor(config.get('colors.blue')) // Set the embed color to a blue color defined in the config.
+            .setDescription(content.replace(`#${ht}`, `**#${ht}**`)) // Replace the hashtag occurance in the message with a bold one. (TODO: may want to make it actually replace the one that it found, may bug out once in a while)
+          if (dguild.hashtags && dguild.hashtags[ht]) {
+            let htchan = guild.channels.get(dguild.hashtags[ht])
+            if (htchan) htchan.send({embed: quote})
+          } else {
+            if (guild.channels.find('name', ht)) return // Prevent people from making hashtags that already exist or hashtags with the same name as a channel that already exists
+            let newChan = await guild.createChannel(ht, 'text', [{
+              id: guild.id,
+              deny: ['SEND_MESSAGES'] // Deny @everyone to send messages in new channel
+            }, {
+              id: client.user.id,
+              allow: ['SEND_MESSAGES'] // Allow bot to send messages in new channel
+            }])
+            await dguild.update({
+              hashtags: Object.assign(dguild.hashtags || {}, {
+                ht: newChan.id
+              })
+            })
+            await newChan.setParent(dguild.hashtagCategory)
+            quote.setFooter('This is the first occurance of the hashtag in the server!')
+            let quoteMsg = await newChan.send({embed: quote})
+            await quoteMsg.pin()
+          }
+        })
+      }
+    } catch(err) {
+      log.error('Failed to do something in message handler')
+      log.error(err)
+    }
   }
 })
 
@@ -40,3 +83,5 @@ client.registry
 
 // Login using the token provided in the configuration
 client.login(config.get('bot.token'))
+
+process.on('unhandledRejection', (err) => console.error(err))
